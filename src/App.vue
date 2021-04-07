6c6569bd03d1ed119ba98c5741350498521c77ba
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2021-04-07 21:55:05 +0100

    slight navbar improvement

--- src/App.vue
+++ src/App.vue
@@ -9,31 +9,17 @@
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item @click="rt_UploadImage">Upload Image</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
 
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
+
+            <b-nav-item-dropdown text="Defect Gallery" left>
+              <b-dropdown-item @click="rt_Defect('Defect1')">Defect1</b-dropdown-item>
+              <b-dropdown-item @click="rt_Defect('Defect2')">Defect2</b-dropdown-item>
             </b-nav-item-dropdown>
 
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
@@ -49,6 +35,9 @@ export default {
   methods: {
     rt_UploadImage(){
       this.$router.push({path: "/Upload_Image"})
+    },
+    rt_Defect(defect_name){
+      this.$router.push({path: "/Defect", query: {defectName: defect_name}})
     }
   }
 }
